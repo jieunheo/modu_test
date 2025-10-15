adcbf9410ddb77fb14a9568486d77d891a5d7fa1
jieunheo <gjekdmlwl@naver.com>
2025-10-15 18:47:41 +0900

    - 실행중인 경우 숫자 변경 불가 - 공통 코드 함수화

--- scripts/script.js
+++ scripts/script.js
@@ -1,85 +1,90 @@
-const hrs = document.getElementById("hrs");
-const min = document.getElementById("min");
-const sec = document.getElementById("sec");
+(() => {
+  const hrs = document.getElementById("hrs");
+  const min = document.getElementById("min");
+  const sec = document.getElementById("sec");
 
-const btnStart = document.getElementById("btn-start");
-const btnStop = document.getElementById("btn-stop");
-const btnReset = document.getElementById("btn-reset");
+  const btnStart = document.getElementById("btn-start");
+  const btnStop = document.getElementById("btn-stop");
+  const btnReset = document.getElementById("btn-reset");
 
-let isRunning = false;
-let timer = null;
-let time = 0;
+  let isRunning = false;
+  let timer = null;
+  let time = 0;
 
-hrs.addEventListener("click", (e) => {
-  isRunning = true;
-  setTime(3600);
-});
-min.addEventListener("click", (e) => {
-  isRunning = true;
-  setTime(60);
-});
-sec.addEventListener("click", (e) => {
-  isRunning = true;
-  setTime(10);
-});
+  hrs.addEventListener("click", (e) => {
+    if (isRunning) return;
+    setTime(3600);
+  });
+  min.addEventListener("click", (e) => {
+    if (isRunning) return;
+    setTime(60);
+  });
+  sec.addEventListener("click", (e) => {
+    if (isRunning) return;
+    setTime(10);
+  });
 
-function setTime(num) {
-  btnStart.disabled = false;
-  btnReset.disabled = false;
+  function setTime(num) {
+    btnStart.disabled = false;
+    btnReset.disabled = false;
 
-  if (num) {
-    time += num; // 1시간
+    if (num) {
+      time += num; // 1시간
+    }
+
+    let h = Math.floor(time / 3600);
+    let m = Math.floor((time - h * 3600) / 60);
+    let s = Math.floor(time - h * 3600 - m * 60);
+    console.log(h, m, s);
+
+    hrs.innerText = h < 10 ? `0${h}` : h;
+    min.innerText = m < 10 ? `0${m}` : m;
+    sec.innerText = s < 10 ? `0${s}` : s;
+
+    if (time === 0 && num === -1) endTimer();
   }
 
-  let h = Math.floor(time / 3600);
-  let m = Math.floor((time - h * 3600) / 60);
-  let s = Math.floor(time - h * 3600 - m * 60);
-  console.log(h, m, s);
+  btnStart.addEventListener("click", () => {
+    if (time === 0) return;
 
-  hrs.innerText = h < 10 ? `0${h}` : h;
-  min.innerText = m < 10 ? `0${m}` : m;
-  sec.innerText = s < 10 ? `0${s}` : s;
+    isRunning = true;
 
-  if (time === 0 && num === -1) endTimer();
-}
+    btnStart.style.display = "none";
+    btnStop.style.display = "flex";
 
-btnStart.addEventListener("click", () => {
-  console.log(time);
-  if (time === 0) return;
+    timer = setInterval(() => {
+      setTime(-1);
+    }, 1000);
+  });
 
-  btnStart.style.display = "none";
-  btnStop.style.display = "flex";
+  btnStop.addEventListener("click", () => {
+    btnStop.style.display = "none";
+    btnStart.style.display = "flex";
 
-  timer = setInterval(() => {
-    setTime(-1);
-  }, 1000);
-});
+    clearInterval(timer);
+    isRunning = false;
+  });
 
-btnStop.addEventListener("click", () => {
-  btnStop.style.display = "none";
-  btnStart.style.display = "flex";
+  btnReset.addEventListener("click", () => {
+    time = 0;
+    clearInterval(timer);
 
-  clearInterval(timer);
-});
+    setTime();
+    resetBtn();
+  });
 
-btnReset.addEventListener("click", () => {
-  time = 0;
-  clearInterval(timer);
+  function endTimer() {
+    alert("Finish");
+    clearInterval(timer);
+    resetBtn();
+  }
 
-  setTime();
+  function resetBtn() {
+    btnStop.style.display = "none";
+    btnStart.style.display = "flex";
+    btnStart.disabled = true;
+    btnReset.disabled = true;
 
-  btnStop.style.display = "none";
-  btnStart.style.display = "flex";
-  btnStart.disabled = true;
-  btnReset.disabled = true;
-});
-
-function endTimer() {
-  alert("Finish");
-  clearInterval(timer);
-
-  btnStop.style.display = "none";
-  btnStart.style.display = "flex";
-  btnStart.disabled = true;
-  btnReset.disabled = true;
-}
+    isRunning = false;
+  }
+})();
